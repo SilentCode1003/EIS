6d60b8bf97d64f9218031d9fe6eccd8a33f26d4b
SilentCode1003 <j0s3ph0r3nc10@gmail.com>
2023-01-13 13:25:15 +0800

    Added Network Repository and Modules

--- routes/repository/networkdb.js
+++ routes/repository/networkdb.js
@@ -1,5 +1,5 @@
 const mysql = require('mysql');
-const model = require('../model/models');
+const model = require('../model/networkmodel');
 require('dotenv').config();
 const crypt = require('./crytography');
 
@@ -50,9 +50,52 @@ exports.Select = (sql, table, callback) => {
                 callback(error, null)
             }
 
-            if (table == 'CyberpowerEquipments') {
-                callback(null, model.CyberpowerEquipments(results));
+            if (table == 'DeployEquipment') {
+                return callback(null, model.DeployEquipment(results));
             }
+
+            if (table == 'MasterClient') {
+                return callback(null, model.MasterClient(results));
+            }
+
+            if (table == 'MasterItem') {
+                return callback(null, model.MasterItem(results));
+            }
+
+            if (table == 'MasterLocation') {
+                return callback(null, model.MasterLocation(results));
+            }
+
+            if (table == 'MasterUser') {
+                return callback(null, model.MasterUser(results));
+            }
+
+            if (table == 'NetworkEquipment') {
+                return callback(null, model.NetworkEquipment(results));
+            }
+
+            if (table == 'PulloutEquipment') {
+                return callback(null, model.PulloutEquipment(results));
+            }
+
+            if (table == 'RequestItemDetails') {
+                return callback(null, model.RequestItemDetails(results));
+            }
+
+            if (table == 'ReturnEquipment') {
+                return callback(null, model.ReturnEquipment(results));
+            }
+
+            if (table == 'TransactionNetworkEquipment') {
+                return callback(null, model.TransactionNetworkEquipment(results));
+            }
+
+            if (table == 'TransactionRequestItemEquipment') {
+                return callback(null, model.TransactionRequestItemEquipment(results));
+            }
+
+
+            callback(null, `No model match please check [${table}] if exist in models`);
         });
 
     } catch (error) {
@@ -161,16 +204,219 @@ exports.SelectResult = (sql, callback) => {
 }
 
 exports.InsertTable = (tablename, data, callback) => {
-    if (tablename == 'transaction_cyberpower') {
-        let sql = `INSERT INTO transaction_cyberpower(
-            tc_transactiondate,
-            tc_requestid,
-            tc_ponumber,
-            tc_drnumber,
-            tc_sinumber,
-            tc_crnumber,
-            tc_remarks,
-            tc_status) VALUES ?`;
+    if (tablename == 'master_item') {
+        let sql = `INSERT INTO master_item(
+            mi_itembrand,  
+            mi_itemmodel,
+            mi_itemtype,
+            mi_createddate,
+            mi_createdby,
+            mi_status) VALUES ?`;
+
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
+
+    if (tablename == 'master_location') {
+        let sql = `INSERT INTO master_location(
+	ml_locationname,
+	ml_createddate,
+	ml_createdby,
+	ml_status) VALUES ?`;
+
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
+
+    if (tablename == 'master_client') {
+        let sql = `INSERT INTO master_client(
+            mc_storecode,
+            mc_storename,
+            mc_createddate,
+            mc_createdby,
+            mc_status) VALUES ?`;
+
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
+
+    if (tablename == 'master_user') {
+        let sql = `INSERT INTO master_user(
+            mu_firstname,
+            mu_lastname,
+            mu_username,
+            mu_password,
+            mu_createddate,
+            mu_createdby,
+            mu_status) VALUES ?`;
+
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
+
+    if (tablename == 'network_equipemnt') {
+        let sql = `INSERT INTO network_equipemnt(
+            ne_brand,
+            ne_model,
+            ne_type,
+            ne_serial,
+            ne_pickupdate,
+            ne_withsim,
+            ne_simserial,
+            ne_site,
+            ne_status) VALUES ?`;
+
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
+
+    if (tablename == 'transaction_network_equipment') {
+        let sql = `INSERT INTO transaction_network_equipment(
+            tne_brand,
+            tne_model,
+            tne_type,
+            tne_serial,
+            tne_pickupdate,
+            tne_withsim,
+            tne_simtype,
+            tne_simserial,
+            tne_site,
+            tne_ticketno,
+            tne_deplotby,
+            tne_deployto,
+            tne_deploydate,
+            tne_pulloutbrand,
+            tne_pulloutmodel,
+            tne_pulloutserial,
+            tne_pulloutwithsim,
+            tne_pulloutsimtype,
+            tne_pulloutsimserial,
+            tne_pulloutby,
+            tne_pulloutfrom,
+            tne_pulloutdate,
+            tne_status) VALUES ?`;
+
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
+
+    if (tablename == 'deploy_equipment') {
+        let sql = `INSERT INTO deploy_equipment(
+            de_deployid,
+            de_brand,
+            de_model,
+            de_serial,
+            de_withsim,
+            de_simtype,
+            de_simserial,
+            de_deploydate,
+            de_deployby,
+            de_deployto) VALUES ?`;
+
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
+
+    if (tablename == 'pullout_equipment') {
+        let sql = `INSERT INTO pullout_equipment(
+            pe_pulloutid,
+            pe_brand,
+            pe_model,
+            pe_serial,
+            pe_withsim,
+            pe_simtype,
+            pe_simserial,
+            pe_pulloutdate,
+            pe_pulloutby,
+            pe_pulloutfrom,
+            pe_status) VALUES ?`;
+
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
+
+    if (tablename == 'return_equipment') {
+        let sql = `INSERT INTO return_equipment(
+            re_returnid,
+            re_brand,
+            re_model,
+            re_serial,
+            re_withsim,
+            re_simtype,
+            re_simserial,
+            re_returndate) VALUES ?`;
+
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
+
+    if (tablename == 'request_item_details') {
+        let sql = `INSERT INTO request_item_details(
+            rid_requestid,
+            rid_requestdate,
+            rid_requestby,
+            rid_store,
+            rid_details,
+            rid_status,) VALUES ?`;
+
+        this.Insert(sql, data, (err, result) => {
+            if (err) {
+                callback(err, null);
+            }
+            callback(null, result)
+        })
+    }
+
+    if (tablename == 'transaction_request_item_equipment') {
+        let sql = `INSERT INTO transaction_request_item_equipment(
+            trie_transactionid,
+            trie_requestid,
+            trie_requestby,
+            trie_requestdate,
+            trie_store,
+            trie_brand,
+            trie_model,
+            trie_serial,
+            trie_withsim,
+            trie_simtype,
+            trie_simserial,
+            trie_status) VALUES ?`;
 
         this.Insert(sql, data, (err, result) => {
             if (err) {
